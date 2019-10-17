515afe72f2cbdfed7dfe81ca2b2a8ab576b4e696
Cassiano Tadao Yasumitsu <cassianotadaoyasumitsu@gmail.com>
2019-10-17 21:57:52 +0900

    feat: API, PropTypes, Repositórios e Issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
     'react/jsx-filename-extension': [
       'warn',
       { extensions: ['.jsx', '.js'] }
